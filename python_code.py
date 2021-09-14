aafaf9ce807fb1399ad0e460d9b03a7d5ff646c4
torinman <77184911+torinman@users.noreply.github.com>
2021-09-14 17:44:55 +1200

    fixed collisions

--- python_code.py
+++ python_code.py
@@ -5,7 +5,7 @@ import math
 import urllib.request
 import colorsys
 import threading
-urls = [] # any set urls you want
+urls = ["http://192.168.0.40/"]#, "http://192.168.0.40/", "http://192.168.0.40/"]  # any set urls you want
 cars = len(urls)
 nauty_wheel = False
 lines = []
@@ -43,15 +43,7 @@ for i in range(cars):
 	prev_wheel_poseses.append([(0, 0), (0, 0), (0, 0), (0, 0)])
 	wheel_poseses.append([(0, 0), (0, 0), (0, 0), (0, 0)])
 	wheel_poseses2.append([(0, 0), (0, 0), (0, 0), (0, 0)])
-def do_crash(line, line2, i):
-	global collide_car, crasheds, crashchannels, lines_in_my_car
-	if (calculateIntersectPoint(line[0], line[1], line2[0], line2[1]) != None):
-		collide_car = True
-		if crasheds[i] >= cars*len(lines_in_my_car):
-			crashchannels[i].play(pygame.mixer.Sound("car_crash_sound.ogg"))
-		crasheds[i] = 0
-	else:
-		crasheds[i] += 1
+
 def calculateGradient(p1, p2):
    if (p1[0] != p2[0]):
        m = (p1[1] - p2[1]) / (p1[0] - p2[0])
@@ -124,10 +116,19 @@ def calculateIntersectPoint(p1, p2, p3, p4):
         return None            
     else:
         return None
-
+def do_crash(line, line2, i):
+	global collide_car, crasheds, crashchannels, lines_in_my_car, lines
+	if (calculateIntersectPoint(line[0], line[1], line2[0], line2[1]) != None):
+		collide_car = True
+		if crasheds[i] >= (cars*len(lines_in_my_car))+200:
+			crashchannels[i].play(pygame.mixer.Sound("car_crash_sound.ogg"))
+		crasheds[i] = 0
+	else:
+		crasheds[i] += 1
 def draw_bg(op):
 	global lines
-	lines = [((pygame.display.get_window_size()[0]/6, pygame.display.get_window_size()[1]/2), ((pygame.display.get_window_size()[0]/6)*5, pygame.display.get_window_size()[1]/2))]
+	lines = [((pygame.display.get_window_size()[0]/6, pygame.display.get_window_size()[1]/2), ((pygame.display.get_window_size()[0]/6)*5, pygame.display.get_window_size()[1]/2)), 
+			 ((0, 0), (400, 400))]
 	surf = pygame.Surface(pygame.display.get_window_size())
 	surf = surf.convert_alpha()
 	surf.fill((BG_COL[0], BG_COL[1], BG_COL[2], op))
@@ -192,31 +193,36 @@ while not done:
 	screen.fill((0, 0, 0))
 
 	#datatups = list((random.randint(-90, 90), random.randint(20, 30)) for i in range(cars)) # Random for when not using the MMU input
-	clock.tick(6000)
+	clock.tick(60)
 	for i in range(cars):
-		speeds[i] += (size_of_car/100)*(datatups[i][1]/50)
+		speeds[i] += (size_of_car/100)*(datatups[i][1]/20)
 		x_speeds[i] = (math.sin(math.radians(player_angles[i]))*speeds[i])
 		y_speeds[i] = (math.cos(math.radians(player_angles[i]))*speeds[i])
 		angles[i] = get_angle((datatups[i][0], speeds[i]))
 		wheel_poseses2[i] = [(xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]-90))*(size_of_car/100)*20), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]-90))*(size_of_car/100)*20)), (xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]+90))*(size_of_car/100)*20), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]+90))*(size_of_car/100)*20)), (xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]-10))*(size_of_car/100)*90), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]-10))*(size_of_car/100)*90)), (xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]+10))*(size_of_car/100)*90), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]+10))*(size_of_car/100)*90))]
 #	screen.blit(text, (0, 0))
 		collide_car = False
-		lines_in_my_car = [((wheel_poseses2[i][2][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10), wheel_poseses2[i][2][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10)), (wheel_poseses2[i][2][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10), wheel_poseses2[i][2][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10))), ((wheel_poseses2[i][3][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10), wheel_poseses2[i][3][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10)), (wheel_poseses2[i][3][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10), wheel_poseses2[i][3][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10))), (wheel_poseses2[i][2], wheel_poseses2[i][3]), (wheel_poseses2[i][0], wheel_poseses2[i][1]), ((xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]+90))*(size_of_car/100)*15), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]+90))*(size_of_car/100)*15)), (xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]))*size_of_car), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]))*size_of_car))), ((xs[i]+x_speeds[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]-90))*(size_of_car/100)*15), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]-90))*(size_of_car/100)*15)) , (xs[i]+y_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]))*size_of_car), ys[i]+(math.cos(math.radians(player_angles[i]+angles[i]))*size_of_car)))    ,    ((wheel_poseses2[i][0][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10), wheel_poseses2[i][0][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10)), (wheel_poseses2[i][0][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10), wheel_poseses2[i][0][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10)), (wheel_poseses2[i][1][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10), wheel_poseses2[i][1][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10)), (wheel_poseses2[i][1][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10), wheel_poseses2[i][1][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10)))]
+		lines_in_my_car = [((wheel_poseses2[i][2][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10), wheel_poseses2[i][2][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10)), (wheel_poseses2[i][2][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10), wheel_poseses2[i][2][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10))), 
+						   ((wheel_poseses2[i][3][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10), wheel_poseses2[i][3][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*-10)), (wheel_poseses2[i][3][0]+(math.sin(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10), wheel_poseses2[i][3][1]+(math.cos(math.radians(player_angles[i]+angles[i]+(datatups[i][0]*-1)))*(size_of_car/100)*10))), 
+						   	(wheel_poseses2[i][2], wheel_poseses2[i][3]), 
+							(wheel_poseses2[i][0], wheel_poseses2[i][1]), 
+						   ((xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]+90))*(size_of_car/100)*15), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]+90))*(size_of_car/100)*15)), (xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]))*size_of_car), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]))*size_of_car))), 
+						   ((xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]-90))*(size_of_car/100)*15), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]-90))*(size_of_car/100)*15)), (xs[i]+x_speeds[i]+(math.sin(math.radians(player_angles[i]+angles[i]))*size_of_car), ys[i]+y_speeds[i]+(math.cos(math.radians(player_angles[i]+angles[i]))*size_of_car))), 
+						   ((wheel_poseses2[i][0][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10), wheel_poseses2[i][0][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10)), (wheel_poseses2[i][0][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10), wheel_poseses2[i][0][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10))), 
+						   ((wheel_poseses2[i][1][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10), wheel_poseses2[i][1][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*-10)), (wheel_poseses2[i][1][0]+(math.sin(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10), wheel_poseses2[i][1][1]+(math.cos(math.radians(player_angles[i]+angles[i]))*(size_of_car/100)*10)))]
 		for linecollide in lines:
 			for (index, line) in enumerate(lines_in_my_car):
 				do_crash(line, linecollide, i)
-				if index == 1 and collide_car:
-					nauty_wheel = True
-				elif index == 1:
-					nauty_wheel = False
+				if index == 1:
+					nauty_wheel = collide_car
 		for j in range(cars):
 			lines_in_their_car = [(wheel_poseses[j][2], wheel_poseses[j][3]), (wheel_poseses[j][0], wheel_poseses[j][1]), ((xs[j]+(math.sin(math.radians(player_angles[j]+90))*(size_of_car/100)*15), ys[j]+(math.cos(math.radians(player_angles[j]+90))*(size_of_car/100)*15)), (xs[j]+(math.sin(math.radians(player_angles[j]))*size_of_car), ys[j]+(math.cos(math.radians(player_angles[j]))*size_of_car))), ((xs[j]+(math.sin(math.radians(player_angles[j]-90))*(size_of_car/100)*15), ys[j]+(math.cos(math.radians(player_angles[j]-90))*(size_of_car/100)*15)), (xs[j]+(math.sin(math.radians(player_angles[j]))*size_of_car), ys[j]+(math.cos(math.radians(player_angles[j]))*size_of_car))),  ((wheel_poseses[j][2][0]+(math.sin(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*-10), wheel_poseses[j][2][1]+(math.cos(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*-10)), (wheel_poseses[j][2][0]+(math.sin(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*10), wheel_poseses[j][2][1]+(math.cos(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*10))), ((wheel_poseses[j][3][0]+(math.sin(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*-10), wheel_poseses[j][3][1]+(math.cos(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*-10)), (wheel_poseses[j][3][0]+(math.sin(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*10), wheel_poseses[j][3][1]+(math.cos(math.radians(player_angles[j]+(datatups[j][0]*-1)))*(size_of_car/100)*10))), ((wheel_poseses[j][1][0]+(math.sin(math.radians(player_angles[j]))*(size_of_car/100)*-10), wheel_poseses[j][1][1]+(math.cos(math.radians(player_angles[j]))*(size_of_car/100)*-10)), (wheel_poseses[j][1][0]+(math.sin(math.radians(player_angles[j]))*(size_of_car/100)*10), wheel_poseses[j][1][1]+(math.cos(math.radians(player_angles[j]))*(size_of_car/100)*10)))]
 			if not i==j:
-				for line in lines_in_my_car:
+				for (index, line) in enumerate(lines_in_my_car):
 					for line2 in lines_in_their_car:
 						do_crash(line, line2, i)
-						if index == 1:
-							nauty_wheel = collide_car
+						if index == 1 and collide_car:
+							nauty_wheel = True
 		if not nauty_wheel:
 			wheel_angles[i] = datatups[i][0]
 		if not(xs[i]+x_speeds[i]<0 or 
